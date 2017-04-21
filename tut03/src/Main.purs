2990182bf16e799de57348809beaf45d2cfd1b2e
Alex Kelley <adkelley@users.noreply.github.com>
2017-04-21 06:35:19 -0700

    tut03 published, bonus result example

--- tut03/src/Main.purs
+++ tut03/src/Main.purs
@@ -52,8 +52,8 @@ result name =
   map (slice 1 0) #
   either (\e -> "No color") toUpper
 
--- This veraition will make your head spin, but it really shows the power of
--- mapping and composition. 
+-- Bonus: This veriation will make your head spin. It really shows the power of
+-- mapping and composition. All in one expression!
 result' :: ColorName -> String
 result' name = either (\e -> "No Color") toUpper $ (slice 1 0) <$> hex <$> (findColor name)
 
