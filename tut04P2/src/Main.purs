58fa50e1c475c591bb2f3e607bb57b783b56f917
Alex Kelley <adkelley@mac.com>
2017-06-11 16:02:14 -0700

    Tut04P2 candidate gold

--- tut04P2/src/Main.purs
+++ tut04P2/src/Main.purs
@@ -1,6 +1,7 @@
 module Main where
 
 import Prelude
+
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log, logShow)
 import Control.Monad.Eff.Exception (EXCEPTION, Error, error, try)
@@ -8,9 +9,13 @@ import Control.Monad.Except (runExcept)
 import Data.Either (Either(..), either)
 import Data.Foreign (unsafeFromForeign)
 import Data.Foreign.JSON (parseJSON)
+import Data.List.NonEmpty (head)
 import Node.Encoding (Encoding(..))
 import Node.FS (FS)
 import Node.FS.Sync (readTextFile)
+
+pathToFile :: String
+pathToFile = "./resources/config.json"
 
 newtype Port = Port { port :: Int }
 instance showPort :: Show Port where
@@ -19,28 +24,23 @@ instance showPort :: Show Port where
 defaultPort :: Port
 defaultPort = Port { port: 3000 }
 
-defaultPortJSON :: String
-defaultPortJSON = "\"port\": 4000"
-
-pathToFile :: String
-pathToFile = "./resources/config.json"
-
 portInRange :: Port -> Either Error Port
 portInRange (Port { port }) =
   if (port >= 1000 && port <= 8888)
-    then Right $ Port { port }
+    then Right $ (Port { port })
     else Left $ error "Port number out of range"
 
 parsePort :: String -> Either Error Port
 parsePort port =
   case parsed of
-    Left _ -> Left $ error "Failed to parse port"
+    Left nel -> Left $ error $ show $ head nel
     Right x -> Right $ unsafeFromForeign x :: Port
   where parsed = runExcept $ parseJSON port
 
 
 chain :: forall a b e. (a -> Either e b) ->  Either e a -> Either e b
 chain f  = either (\e -> Left e) (\x -> (f x))
+
 
 getPort :: forall eff. Eff (fs :: FS, exception :: EXCEPTION | eff) Port
 getPort =
@@ -50,14 +50,25 @@ getPort =
   either (\_ -> defaultPort) id >>>
   pure
 
+-- Instead or ignoring errors and returning defaultPort, try creating a JSON string
+-- error or read file error and then use this modified getport function below to log
+-- the error to the console.
+
+getPort' :: forall eff. Eff (fs :: FS, exception :: EXCEPTION | eff) (Either Error Port)
+getPort' =
+  (try $ readTextFile UTF8 pathToFile) >>=
+  chain parsePort >>>
+  chain portInRange >>>
+  pure
+
 
 main :: forall e. Eff (console :: CONSOLE, fs :: FS, exception :: EXCEPTION | e) Unit
 main = do
   log "Use chain for composable error handling with nested Eithers"
 
-    -- Code Example 1: using bind and bundFlipped respectively
+  -- Code Example 1: using bind and bundFlipped respectively
   (try $ readTextFile UTF8 pathToFile) >>= logShow
   logShow =<< (try $ readTextFile UTF8 pathToFile)
 
     -- Code Example 2
-  logShow =<< getPort
+  getPort >>= logShow
--- tut05/src/Main.purs
+++ tut05/src/Main.purs
@@ -1,10 +1,101 @@
 module Main where
 
 import Prelude
+
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.Either (Either(..), either)
+import Data.Foreign (Foreign, isNull, isUndefined, toForeign, unsafeFromForeign)
+import Data.Maybe (Maybe(..), fromJust, isNothing)
+import Partial.Unsafe (unsafePartial)
+
+foreign import null :: Foreign
+
+type Address =
+ { street :: Maybe String
+ , city :: Maybe String
+ , zip :: Maybe String
+ }
+
+type User =
+  { name :: String
+  , address :: Maybe Address
+  , premium :: Boolean
+  , preferences :: Maybe String
+  }
+
+fromNothing :: forall a. Maybe a -> Either (Maybe a) (Maybe a)
+fromNothing value =
+  if (isNothing value)
+   then Left value
+   else Right value
+
+chain :: forall a b e. (a -> Either e b) ->  Either e a -> Either e b
+chain f  = either (\e -> Left e) (\x -> (f x))
+
+fromNullable :: Foreign -> Either Foreign Foreign
+fromNullable value =
+  if (isNull value || isUndefined value)
+   then Left value
+   else Right value
+
+-- Example 1
+openSite :: Foreign -> String
+openSite =
+  fromNullable >>>
+  either
+    (\_ -> "showLogin")
+    (\user -> "renderPage: " <> (unsafeFromForeign user :: String))
+
+-- Example 2
+defaultPrefs :: String
+defaultPrefs = "defaultPrefs"
+
+getPrefs :: User -> String
+getPrefs =
+  isPremium >>>
+  map (_.preferences) >>>
+  either
+    (\_ -> defaultPrefs)
+    (\prefs -> "loadPrefs: " <> (unsafePartial $ fromJust prefs))
+  where
+    isPremium :: User -> Either String User
+    isPremium user =
+      if (user.premium )
+        then Right user
+        else Left "not premimum"
+
+-- Example 3
+-- streetName :: Maybe Address -> String
+-- streetName address =
+--   fromNothing address #
+--   chain (\address -> fromNothing address.street) #
+--   either (\_ -> "no street") (\name -> (unsafePartial $ fromJust name))
+--
+-- streetName :: Address -> String
+-- streetName address =
+--   fromNothing address.street #
+--   chain (\street -> fromNothing street) #
+--   either (\_ -> "no street") (\name -> (unsafePartial $ fromJust name))
 
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  log "Hello sailor!"
+  log "A collection of Either examples"
+
+  log "Example 1"
+  log $ openSite $ toForeign "Fred Flinstone"
+  log $ openSite null
+
+  let address1 = { street: Just "main street", city: "any city", zip: "zip"}
+  let user1 = { name: "fred", address: Nothing, premium: true, preferences: Just "premium prefs" }
+  let user2 = { name: "ethel", address: Nothing, premium: false, preferences: Nothing }
+  log "Example 2"
+  log $ getPrefs user1
+  log $ getPrefs user2
+
+  log "Example 3"
+  log "Example 4"
+  log "Example 5"
+  log "Example 6"
+  log "Game Over"
