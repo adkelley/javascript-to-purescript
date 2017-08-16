1b4fb172ce63012a1c5e1acf7b3e046bfba7540c
Alex Kelley <adkelley@mac.com>
2017-08-16 11:08:32 -0700

    Added First example

--- tut08/src/Main.purs
+++ tut08/src/Main.purs
@@ -5,6 +5,8 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log, logShow)
 import Data.Foldable (foldr)
+import Data.Maybe (Maybe(..))
+import Data.Maybe.First (First(..))
 import Data.Monoid (mempty)
 import Data.Monoid.Additive (Additive(..))
 import Data.Monoid.Conj (Conj(..))
@@ -15,7 +17,11 @@ main = do
   log "Ensure failsafe combination using monoids"
   -- logShow $ (Additive 1) <> (Additive 2) <> (Additive 3) <> mempty
   logShow $ foldr (<>) mempty $ map Additive [1, 2, 3]
-  -- (Conj true) <> (Conj false) <> mempty
+  -- (Conj true) <> (Conj false) <> Conj true <> mempty
   logShow $ foldr (<>) mempty $ map Conj [true, false, true]
+  -- logShow $ (Conj true) <> (Conj true) <> (Conj true) <> mempty
   logShow $ foldr (<>) mempty $ map Conj [true, true, true]
-  -- logShow $ (Conj true) <> (Conj true) <> mempty
+  -- logShow First (Just 1) <> First (Just 2) <> First (Just 3) <> mempty
+  logShow $ foldr (<>) mempty $ map First [Just 1, Just 2, Just 3]
+  -- logShow First Nothing <> First Nothing <> First (Just 3) <> mempty
+  logShow $ foldr (<>) mempty $ map First [Nothing, Nothing, Just 3]
