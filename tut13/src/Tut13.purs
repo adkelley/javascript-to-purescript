391ee5da03a3aa7da4c49954f1bb5a223099d4a7
Alex Kelley <adkelley@mac.com>
2018-05-21 13:13:24 -0700

    Task, TaskE now distinct

--- tut13/src/Tut13.purs
+++ tut13/src/Tut13.purs
@@ -2,7 +2,7 @@ module Tut13 (app) where
 
 import Prelude
 
-import Control.Monad.Aff (Aff, nonCanceler)
+import Control.Monad.Aff (nonCanceler)
 import Control.Monad.Aff.Console (CONSOLE, log)
 import Control.Monad.Eff.Console (log) as Console
 import Control.Monad.Eff.Exception (try)
@@ -12,7 +12,7 @@ import Data.String.Regex.Flags (global)
 import Node.Encoding (Encoding(..))
 import Node.FS (FS)
 import Node.FS.Sync (readTextFile, writeTextFile)
-import Control.Monad.Task (Task, newTask, rej, res, toAff)
+import Control.Monad.Task (Task, TaskE, newTask, rej, res, toAff)
 
 pathToFile :: String
 pathToFile = "./resources/config.json"
@@ -20,7 +20,7 @@ pathToFile = "./resources/config.json"
 readFile_
   :: ∀ e
    . Encoding → String
-   → Task String (fs :: FS, console :: CONSOLE | e) String
+   → TaskE String (fs :: FS, console :: CONSOLE | e) String
 readFile_ enc filePath =
   newTask $
   \cb -> do
@@ -32,7 +32,7 @@ readFile_ enc filePath =
 writeFile_
   :: ∀ e
    . Encoding → String → String
-   → Task String (fs :: FS, console :: CONSOLE | e)  String
+   → TaskE String (fs :: FS, console :: CONSOLE | e)  String
 writeFile_ enc filePath contents =
   newTask $
   \cb -> do
@@ -49,7 +49,7 @@ newContents s =
   where regexp = regex "8" global
 
 
-app :: ∀ e. Aff (console :: CONSOLE, fs :: FS | e) Unit
+app :: ∀ e. Task (console :: CONSOLE, fs :: FS | e) Unit
 app = do
   result ← toAff $
    readFile_ UTF8 pathToFile #
