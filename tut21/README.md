829d9b36f5c092a907d4ada8d4d44a9545728e1d
Alex Kelley <adkelley@mac.com>
2019-02-09 18:09:20 -0800

    code completed

--- tut21/README.md
+++ tut21/README.md
@@ -10,7 +10,3 @@
 > [Index](https://github.com/adkelley/javascript-to-purescript/tree/master/md) | [<< Introduction](https://github.com/adkelley/javascript-to-purescript) [< Tutorial 20](https://github.com/adkelley/javasc-to-purescript/tree/master/tut20) | [> Tutorial 21](https://github.com/adkelley/javascript-to-purescript/tree/master/tut21)
 
 
-## Navigation
-[<--](https://github.com/adkelley/javascript-to-purescript/tree/master/tut20) **Tutorials** [-->](https://github.com/adkelley/javascript-to-purescript)
-
-You may find that the README for the next tutorial is under construction. But if you're an eager beaver and would like to look ahead, then all the of code samples from Brian's [videos](https://egghead.io/courses/professor-frisby-introduces-composable-functional-javascript) have been ported to PureScript already. But I may amend them as I write the accompanying tutorial markdown.  
--- tut21/src/Main.purs
+++ tut21/src/Main.purs
@@ -2,28 +2,39 @@ module Main where
 
 import Prelude
 
+import Control.Apply (lift2)
+import Control.Monad.Task (TaskE, fork, newTask, res, rej)
+import Data.Either (Either(..), either)
 import Effect (Effect)
+import Effect.Aff (launchAff, nonCanceler)
+import Effect.Class.Console (log) as Console
 import Effect.Console (log)
+import Effect.Timer (setTimeout)
 
--- foreign import data TIMEOUT :: Effect
+type Project = { id :: Int, title :: String }
 
--- type TOF eff = Eff (timeout :: TIMEOUT | eff)
 
-type Milliseconds = Int
+makeProject :: Int → String → Effect (Either String Project)
+makeProject id title =
+  pure $ Right { id: id, title: title }
 
-foreign import setTimeout
-  :: Milliseconds
-  -> Effect Unit
-  -> Effect Unit
 
-type Report =
-  { title :: String
-  , id    :: Int
-  }
+find :: Int → TaskE String Project
+find id =
+  newTask $ \cb → do
+    let project =
+          makeProject id ("Project: " <> (show id)) >>=
+          \p → cb $ either (\e → rej $ show e) (\s → res s) p
+    _ <- setTimeout 100 project
+    pure $ nonCanceler
 
-reportHeader :: Report -> Report -> String
+reportHeader :: Project -> Project -> String
 reportHeader p1 p2 = "Report: " <> p1.title <> " compared to " <> p2.title
+
 
 main :: Effect Unit
 main = do
   log "Write applicative for concurrent actions"
+  void $ launchAff $
+    lift2 (\p1 p2 → reportHeader p1 p2) (find 20) (find 39) #
+    fork (\e → Console.log $ "error: " <> e) (\p → Console.log p)
