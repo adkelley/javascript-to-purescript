fd6658b0d249cc0d3565a8d3b43ee9e8cff58881
Alex Kelley <adkelley@mac.com>
2019-02-10 13:43:19 -0800

    final code

--- tut21/src/Main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-exports.setTimeout = function(ms) {
-  return function(fn) {
-    return function() {
-      return setTimeout(fn, ms);
-    }
-  }
-}
--- tut21/src/Main.purs
+++ tut21/src/Main.purs
@@ -5,6 +5,7 @@ import Prelude
 import Control.Apply (lift2)
 import Control.Monad.Task (TaskE, fork, newTask, res, rej)
 import Data.Either (Either(..), either)
+import Data.Foldable (foldr)
 import Effect (Effect)
 import Effect.Aff (launchAff, nonCanceler)
 import Effect.Class.Console (log) as Console
@@ -13,28 +14,40 @@ import Effect.Timer (setTimeout)
 
 type Project = { id :: Int, title :: String }
 
-
-makeProject :: Int → String → Effect (Either String Project)
-makeProject id title =
-  pure $ Right { id: id, title: title }
-
-
-find :: Int → TaskE String Project
-find id =
-  newTask $ \cb → do
-    let project =
-          makeProject id ("Project: " <> (show id)) >>=
-          \p → cb $ either (\e → rej $ show e) (\s → res s) p
-    _ <- setTimeout 100 project
-    pure $ nonCanceler
+-- | Simulate a database query using setTimeout
+-- | to return a record with an id and a title
+dbFind :: Int → TaskE String Project
+dbFind id =
+  let
+    query :: Int → Effect (Either String Project)
+    query id_ =
+      pure $ Right { id: id_, title: "Project: " <> (show id)}
+  in
+   newTask $ \cb → do
+     let response =
+           query id >>= \r →
+             cb $ either (\e → rej $ show e) (\s → res s) r
+     _ <- setTimeout 100 response
+     pure $ nonCanceler
 
 reportHeader :: Project -> Project -> String
-reportHeader p1 p2 = "Report: " <> p1.title <> " compared to " <> p2.title
+reportHeader p1 p2 =
+  foldr (<>) "" ["Report: ", p1.title, " compared to ", p2.title]
 
 
 main :: Effect Unit
 main = do
   log "Write applicative for concurrent actions"
   void $ launchAff $
-    lift2 (\p1 p2 → reportHeader p1 p2) (find 20) (find 39) #
-    fork (\e → Console.log $ "error: " <> e) (\p → Console.log p)
+    let s = "\nTwo sequential finds:\n"
+    in
+      (dbFind 20) >>=
+        (\p1 → (dbFind 8) >>=
+           \p2 → pure $ reportHeader p1 p2)
+      # fork (\e → Console.log $ "error: " <> e) (\p → Console.log (s <> p))
+
+  void $ launchAff $
+    let s = "\nRewritten using two concurrent finds:\n"
+    in
+     lift2 (\p1 p2 → reportHeader p1 p2) (dbFind 20) (dbFind 8)
+     # fork (\e → Console.log $ "error: " <> e) (\p → Console.log (s <> p))
