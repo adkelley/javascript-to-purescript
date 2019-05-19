e77401b94ecee39b280cd6bb3a8f9d73de6b3540
Alex Kelley <adkelley@mac.com>
2019-05-19 16:38:32 -0700

    add: tut22 code

--- tut22/src/Main.purs
+++ tut22/src/Main.purs
@@ -1,9 +1,40 @@
 module Main where
 
 import Prelude
+
+import Control.Monad.Task (TaskE, fork, newTask, rej, res)
+import Data.Either (Either, either)
+import Data.Foldable (foldl)
+import Data.Traversable (traverse)
 import Effect (Effect)
+import Effect.Aff (launchAff, nonCanceler)
+import Effect.Class.Console (log, errorShow) as Console
 import Effect.Console (log)
+import Effect.Exception (Error, try)
+import Node.Encoding (Encoding(..))
+import Node.FS.Sync (readTextFile)
+
+pathToFile :: String
+pathToFile = "./resources/"
+
+files :: Array String
+files = map (\x → pathToFile <> x) ["Box.purs", "config.json"]
+
+taskReadTextFile :: String → TaskE Error String
+taskReadTextFile fname =
+  let
+    tryReadTextFile :: String → Effect (Either Error String)
+    tryReadTextFile fname_ = try $ readTextFile UTF8 fname_
+  in
+    newTask $ \callback → do
+      tryReadTextFile fname >>= \r →
+        callback $ either (\e → rej e) (\s → res s) r
+      pure $ nonCanceler
+
 
 main :: Effect Unit
 main = do
-  log "Hello Tut22!"
+  log "Tutorial 22: Leapfrogging types with Traverse"
+  void $ launchAff $
+    traverse (\x → taskReadTextFile x) files
+    # fork (\e → Console.errorShow e) (\r → Console.log $ foldl (<>) "" r)
