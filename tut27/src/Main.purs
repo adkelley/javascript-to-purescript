911920e058b0ce5f00b3ab87f17ba8dd514dcd7d
Alex Kelley <adkelley@mac.com>
2019-10-19 12:24:45 -0700

    tut27: src first pass

--- tut27/src/Main.purs
+++ tut27/src/Main.purs
@@ -2,14 +2,14 @@ module Main where
 
 import Prelude
 
-import Control.Monad.Task (TaskE, fork, newTask, rej, res, taskOf, taskRejected)
+import Control.Monad.Task (TaskE, fork, newTask, rej, res, taskOf)
 import Data.Array (drop, length)
 import Data.Either (Either(..), either)
 import Data.Traversable (traverse)
 import Effect (Effect)
 import Effect.Aff (launchAff, nonCanceler)
-import Effect.Class.Console (error, log, logShow) as Console
-import Effect.Console (log, logShow)
+import Effect.Class.Console (error, logShow) as Console
+import Effect.Console (log)
 import Node.Process (argv)
 
 type Error = String
@@ -23,11 +23,11 @@ names =
   let
     checkArgs :: Effect (Either Error Names)
     checkArgs = do
-      args <- argv >>= \xs -> pure $ drop 2 xs
+      args <- (drop 2) <$> argv
       pure $
-        if (length args < 2)
-          then Left "must input >= 2 names"
-          else Right args
+        if (length args > 0)
+          then Right args
+          else Left "you must enter at least one name"
   in
     newTask $ \callback -> do
       checkArgs >>= \args ->
@@ -38,13 +38,13 @@ findArtist :: String -> TaskE Error Artist
 findArtist name = taskOf name
 
 relatedArtists :: Id -> TaskE Error RelatedArtists
-relatedArtists id = taskOf ["Alex", "Billy"]
+relatedArtists id = taskOf ["Oasis", "Blur"]
 
 artistId :: Artist -> TaskE Error Int
-artistId artist = taskRejected "no id"
+artistId _ = taskOf 10
 
 related :: String -> TaskE Error RelatedArtists
-related name = findArtist name >>= artistId >>= relatedArtists
+related name = relatedArtists =<< artistId =<< findArtist name
 
 main :: Effect Unit
 main = do
