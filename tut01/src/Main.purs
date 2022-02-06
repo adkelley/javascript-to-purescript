3bef8fa00134fa4bc6da646fecf9acb5aee07b23
Alex Kelley <alexkelley@Alexs-MBP.webpass.net>
2022-02-06 13:26:41 -0800

    Bumped PureScript compiler and Spago versions

--- tut01/src/Main.purs
+++ tut01/src/Main.purs
@@ -1,4 +1,6 @@
-module Main where
+module Main
+  ( nextCharForNumberString
+  ) where
 
 import Prelude
 
@@ -13,14 +15,18 @@ import Effect.Console (log)
 
 -- Javascript - const Box = x => ({})
 newtype Box a = Box a
+
 -- Javascript - map: f => (f(x))
 instance functorBox :: Functor Box where
   map f (Box x) = Box (f x)
+
 -- Javascript - fold: f => f(x)
 instance extendBox :: Extend Box where
   extend f m = Box (f m)
+
 instance comonadBox :: Comonad Box where
   extract (Box x) = x
+
 -- Javascript - inspect: () => 'Box($(x))'
 instance showBox :: Show a => Show (Box a) where
   show (Box a) = "Box(" <> show a <> ")"
@@ -29,32 +35,34 @@ instance showBox :: Show a => Show (Box a) where
 -- This is suboptimal because its hard to follow
 nextCharForNumberString' :: String -> String
 nextCharForNumberString' str =
-  singleton(fromMaybe ' ' (fromCharCode(fromMaybe 0 (fromString(trim(str))) + 1)))
+  singleton (fromMaybe ' ' (fromCharCode (fromMaybe 0 (fromString (trim (str))) + 1)))
 
 -- Composition using oridinary functions.  This is simple, to read, write
 -- use and reason about than bundled parenthesis.  See the ReadMe in Tutorial 2
 -- for more information on this topic
 nextCharForNumberString'' :: String -> String
 nextCharForNumberString'' =
-  trim >>>
-  fromString >>>
-  fromMaybe 0 >>>
-  (+) 1 >>>
-  fromCharCode >>>
-  fromMaybe ' ' >>>
-  singleton
+  trim
+    >>> fromString
+    >>> fromMaybe 0
+    >>> (+) 1
+    >>> fromCharCode
+    >>> fromMaybe ' '
+    >>>
+      singleton
 
 -- But when mixing categories (i.e., Box, Maybe), we'll often use
 -- composition by putting s into a box and mapping over it
 nextCharForNumberString :: String -> String
 nextCharForNumberString str =
-  Box str #
-  map trim #
-  map (\s -> fromMaybe 0 $ fromString s) #
-  map (\i -> i + 1) #
-  map (\i -> fromMaybe ' ' $ fromCharCode i) #
-  map (\c -> toLower $ singleton c) #
-  extract
+  Box str
+    # map trim
+    # map (\s -> fromMaybe 0 $ fromString s)
+    # map (\i -> i + 1)
+    # map (\i -> fromMaybe ' ' $ fromCharCode i)
+    # map (\c -> toLower $ singleton c)
+    #
+      extract
 
 main :: Effect Unit
 main = do
